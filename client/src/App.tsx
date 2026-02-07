fea81ad8b8e87afffa2942fdaa72882741821814
aexli2 <159849911+aexli2@users.noreply.github.com>
2026-02-07 11:47:48 -0600

    Added Title page and title transition format

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,49 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import main3d from './3d/main'
 
+
+
 export default function App() {
     const CanvasRef = useRef<HTMLCanvasElement | null>(null)
+
+    const[,Title] = useState(28);
+
+
+    const AreaRef=useRef<HTMLDivElement>(null);//For titleTransition
+
+    const ToSecondLevel=useRef<HTMLDivElement>(null);//For after titletransition
+    
 
     useEffect(() => {
         const engine3d = main3d(CanvasRef.current as HTMLCanvasElement)
     
         // dispose 3d engine
+        console.log(AreaRef.current);
+        
+
         return () => engine3d()
+        
     }, [])
+
+
+    function TitleTransition(){
+        AreaRef.current!.style.backgroundImage='url("/SolitaryTitleTransition.gif")';
+    }
+    
 
     return <>
         <canvas ref={CanvasRef} id="threejs"></canvas>
-        <div className="area-2d">
+        
+        <div className="TitleBackground" ref={AreaRef} >
+            
             {/* 2d stuff goes here vvvvvv */}
-            2d stuff
+
+            <button className="TitleButton"
+            onClick={TitleTransition}>     </button>
+            
+
+
         </div>
     </>
 }
