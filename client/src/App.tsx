622f1bc162ef10efb4812c915f571e0065506573
aexli2 <159849911+aexli2@users.noreply.github.com>
2026-02-07 12:39:16 -0600

    Added title transition to next scene

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import main3d from './3d/main'
+import './App.css'
 
 
 
@@ -11,6 +12,9 @@ export default function App() {
 
 
     const AreaRef=useRef<HTMLDivElement>(null);//For titleTransition
+    const TitleText=useRef<HTMLDivElement>(null);//For titletext
+    const threeSecTimer=useRef<HTMLDivElement>(null);//For creating a 3 second timer
+    const fader=useRef<HTMLDivElement>(null);//Permamently fades a div element
 
     const ToSecondLevel=useRef<HTMLDivElement>(null);//For after titletransition
     
@@ -29,22 +33,39 @@ export default function App() {
 
     function TitleTransition(){
         AreaRef.current!.style.backgroundImage='url("/SolitaryTitleTransition.gif")';
+        AreaRef.current!.style.animation='fade 1s linear forwards';
+        AreaRef.current!.style.opacity='0';
+
+
+
     }
+
+
+
     
+
+
+
+
 
     return <>
         <canvas ref={CanvasRef} id="threejs"></canvas>
         
         <div className="TitleBackground" ref={AreaRef} >
             
+            
             {/* 2d stuff goes here vvvvvv */}
 
             <button className="TitleButton"
-            onClick={TitleTransition}>     </button>
+            onClick={TitleTransition}></button>
             
+            <div className="TitleText" ref={TitleText} style={{width: "70%", display: 'flex', position: "fixed",right: "20%", alignItems: "center",justifyContent: "center", fontSize: "500%", fontFamily: 'sans-serif',fontStyle: 'italic',fontWeight:"100%"}}>Lost Solitary
 
+            </div>
 
         </div>
+
+        
     </>
 }
 
